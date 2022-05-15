7266e5803d9009d5dc96077eebeaac9c77e18ce3
Wilk Rafał <61004695+rafalwilk4ti1@users.noreply.github.com>
2022-05-15 21:07:31 +0200

    Madlib Game
    Second one program type of getting user's input and perform normal printing. Review once again similar logic, and apply into very popular game.

---
+++ madlib_game/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color: \t");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a noun: \t");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("\nRoses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I love " + celebrity);
+
+
+            Console.ReadLine();
+        }
+    }
+}
