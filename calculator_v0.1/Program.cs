0f33bf84188a1f81360c787805e4de5aac3bae48
Wilk Rafał <61004695+rafalwilk4ti1@users.noreply.github.com>
2022-05-15 15:23:43 +0200

    Basic calculator
    Creating basic program helps to get really steady foundation.
    In this program I've learnt how to get inputs from user, how to convert str to int, int to double etc. and also how to apply pretty simple logic into my program.

---
+++ calculator_v0.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: \t\t");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: \t\t");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Addition: \t\t\t" + (num1 + num2));
+            Console.WriteLine("Substraction: \t\t\t" + (num1 - num2));
+            Console.WriteLine("Multiplication: \t\t" + (num1 * num2));
+            Console.WriteLine("Division: \t\t\t" + (num1 / num2));
+
+
+            Console.ReadLine();
+        }
+    }
+}
